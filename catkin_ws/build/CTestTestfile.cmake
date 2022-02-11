38f2d62008758c61036fa6da2e48d8ccd2cf9ce2
eggbread <gyeongmin251@gmail.com>
2022-02-11 19:20:24 +0900

    rm directory

--- catkin_ws/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/episci/Projects/network_tester/catkin_ws/src
-# Build directory: /home/episci/Projects/network_tester/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("cpp")
-subdirs("python")
